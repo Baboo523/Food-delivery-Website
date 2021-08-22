609c9eabd1213a6494f7b0a8aa646f671fc56026
Baboo523 <babooheerani999@gmail.com>
2021-08-22 17:13:46 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+const sigup = ()=>
+{
+    var email = document.getElementById("email").value
+    var name = document.getElementById("name").value
+    var password = document.getElementById("password").value
+    var phone= document.getElementById('phone').value;
+    var select = document.getElementById("type").value
+    console.log(email,name,password,select)
+
+    if(email==''||name=='' ||password=="" || phone=="")
+    {
+        alert("Enter Correct Values")
+    }
+    else
+    {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((result) => 
+          {
+            var user = result.user;
+            console.log("User :",user)
+            console.log("User Uid:",user.uid)
+            var obj = 
+            {
+                Name : name,
+                email : email,
+                password :password,
+                type : select,
+                uid : user.uid
+            }
+
+            firebase.database().ref(`/${select}`).child(user.uid).set(obj)
+            .then((data)=>
+            {
+            window.location='index.html'
+            })
+            // window.location='login.html'
+          })
+          .catch((error) => 
+          {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+            // ..
+          });
+    }
+}
+
+
+const sigin = ()=>
+{
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+    console.log(email,password);
+    if(email=='' ||password=="" )
+    {
+        alert("Enter Correct Values")
+    }
+    else
+    {
+        firebase.auth().signInWithEmailAndPassword(email, password )
+        .then((result) => 
+        {
+          var user = result.user;
+          console.log(user)
+          console.log("User Email :",user.email)
+          console.log("User Uid :",user.uid)
+
+          localStorage.setItem('Current_user Uid',user.uid)
+
+          window.location='index.html'
+
+        //   firebase.database().ref().child('Resturant')
+        //   .orderByChild('uid')
+        //   .equalTo(user.uid)
+        //   .once('value')
+        //   .then((snap)=>{
+        //       console.log("snap",snap.toJSON())
+
+        //   })
+
+
+
+        //   var obj = {
+        //       Name : name,
+        //       email : email,
+        //       password :password,
+        //       type : select,
+        //       uid : user.uid
+        //   }
+
+        //   firebase.database().ref(`/${select}`).child(user.uid).set(obj)
+        })
+        .catch((error) => 
+        {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorMessage)
+          // ..
+        });
+    }
+} 
+// console.log("APP.JS",localStorage.getItem('USer_Email'))
---
+++ js/app2.js
@@ -0,0 +1,2 @@
+console.log(localStorage.getItem('Current_user Uid'))
+var a = localStorage.getItem('Current_user Uid')
